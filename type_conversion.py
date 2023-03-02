a5425e4940e4c72874f3884a23d81908fc53b5d3
Salait Sudhakar S <salaitsudhakar.s@gmail.com>
2023-03-02 20:16:47 +0530

    PODMAS RULE ADDED
    python expression evaluated. and podmas rule corrected. division has higher precedence than multiplication.

--- type_conversion.py
+++ type_conversion.py
@@ -1,4 +1,4 @@
-# input()
+# input():
 """ input() function used to get any details from user.
 The value user enters after prompt is always a str object type.
 """
@@ -32,9 +32,34 @@ str() - to convert into string object
 bool() - to convert into Boolean object
 """
 
-# type()
+# type() :
 """ type() functions used to check the type of any value or Variable. """
 print(type(10))       # <class 'int'>
 print(type(18.0))     # <class 'float'>
 print(type("Sachin")) # <class 'str'>
 print(type(12 > 15))  # <class 'bool'>
+
+
+# Expression :
+""" In Python, expression are evaluated by operator precedence, which determines the order in which operators are evaluated 
+P - Parentheses ()
+E - Exponentiation **
+D - Division / -> //
+M - Multiplication * -> %
+A - Addition +
+S - Subtraction -
+In above order operators are evaluated.
+"""
+# Example :
+result = (10 - 3) ** 2 + 123 / 3 * 2
+print(result)
+
+""" Always values inside a parentheses first evaluated. Then exponentiation, division etc... 
+as per above PEDMAS Rule.
+let me explain above example.
+step1 : first the expression inside a parentheses evaluated. 7 ** 2 + 123 / 3 * 2
+step2 : exponentiation operation evaluated. 49 + 123 / 3 * 2
+step3 : then division - 49 + 41.0 * 2 
+step4 : multiplication -   49 + 82.0 
+final step - 131.0
+"""
